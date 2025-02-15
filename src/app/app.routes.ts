1605fe0bcf7c046d0ccdeceae969def5085254d0
Crisz360 <40069771+Crisz360@users.noreply.github.com>
2025-02-15 15:11:23 -0600

    Calculadora

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+export const routes: Routes = [
+    { path: '', component: CalculadoraComponent }
+];
